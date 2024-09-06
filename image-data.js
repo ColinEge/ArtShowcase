70632e80ef488fe0ea68da46c8f5b002dbe08249
ColinEge <github@colinegerton.co.uk>
2024-09-06 01:00:55 +0100

    initial commit

---
+++ image-data.js
@@ -0,0 +1,81 @@
+const imageCollections = [
+    {
+        id: 1,
+        name: "Paintings",
+        previewImage: "./collections/paintings/1.jfif",
+        images: [
+            "./collections/paintings/1.jfif",
+            "./collections/paintings/2.jfif",
+            "./collections/paintings/3.jfif",
+            "./collections/paintings/4.jfif",
+            "./collections/paintings/5.jfif",
+            "./collections/paintings/6.jfif",
+            // more images...
+        ]
+    },
+    {
+        id: 2,
+        name: "Commissions",
+        previewImage: "./collections/commissions/1.jfif",
+        images: [
+            "./collections/commissions/1.jfif",
+            "./collections/commissions/2.jfif",
+            "./collections/commissions/3.jfif",
+            "./collections/commissions/4.jfif",
+            // more images...
+        ]
+    },
+    {
+        id: 3,
+        name: "City",
+        previewImage: "./collections/city/1.jfif",
+        images: [
+            "./collections/city/1.jfif",
+            "./collections/city/2.jfif",
+            "./collections/city/3.jfif",
+            "./collections/city/4.jfif",
+            "./collections/city/5.jfif",
+            "./collections/city/6.jfif",
+            // more images...
+        ]
+    },
+    {
+        id: 4,
+        name: "Kevin",
+        previewImage: "./collections/kevin/1.jfif",
+        images: [
+            "./collections/kevin/1.jfif",
+            "./collections/kevin/2.jfif",
+            "./collections/kevin/3.jfif",
+            "./collections/kevin/4.jfif",
+            "./collections/kevin/5.jfif",
+            // more images...
+        ]
+    },
+    {
+        id: 5,
+        name: "Landscapes",
+        previewImage: "./collections/landscapes/1.jfif",
+        images: [
+            "./collections/landscapes/1.jfif",
+            "./collections/landscapes/2.jfif",
+            "./collections/landscapes/3.jfif",
+            // more images...
+        ]
+    },
+    {
+        id: 6,
+        name: "Portraits",
+        previewImage: "./collections/portraits/1.jfif",
+        images: [
+            "./collections/portraits/1.jfif",
+            "./collections/portraits/2.jfif",
+            "./collections/portraits/3.jfif",
+            // more images...
+        ]
+    },
+    // more collections...
+];
+
+
+
---
+++ index.js
@@ -0,0 +1,199 @@
+//
+// Carousel
+//
+
+const carousel = document.querySelector('.story-carousel');
+!function () {
+    let isDragging = false;
+    let startPos = 0;
+    let currentTranslate = 0;
+    let prevTranslate = 0;
+    let animationID;
+
+    // Track start mouse or finger horizontal position
+    const getPositionX = (event) => event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+
+    // Request refreshes more often to prevent jitter
+    const animation = () => {
+        if (isDragging) requestAnimationFrame(animation);
+    };
+
+    // Keep track of where touch/mouse started
+    const touchStart = (event) => {
+        isDragging = true;
+        startPos = getPositionX(event);
+        animationID = requestAnimationFrame(animation);
+        carousel.classList.add('grabbing');
+    };
+
+    // TranslateX based on mouse position difference
+    const touchMove = (event) => {
+        if (!isDragging) return;
+        const currentPosition = getPositionX(event);
+        currentTranslate = prevTranslate + currentPosition - startPos;
+    };
+
+    // Stop moving things when touch ends
+    const touchEnd = () => {
+        isDragging = false;
+        cancelAnimationFrame(animationID);
+        prevTranslate = currentTranslate;
+        carousel.classList.remove('grabbing');
+    };
+
+    // Add event listeners for desktop and mobile
+    carousel.addEventListener('mousedown', touchStart);
+    carousel.addEventListener('touchstart', touchStart);
+    carousel.addEventListener('mousemove', touchMove);
+    carousel.addEventListener('touchmove', touchMove);
+    carousel.addEventListener('mouseup', touchEnd);
+    carousel.addEventListener('touchend', touchEnd);
+    carousel.addEventListener('mouseleave', touchEnd);
+    carousel.addEventListener('touchcancel', touchEnd);
+
+}();
+
+//
+// Modal
+//
+
+const modal = document.getElementById('imageModal');
+!function () {
+    const modalClose = document.querySelector('.modal-close');
+
+    // Close modal when clicking the close button
+    modalClose.addEventListener('click', () => {
+        modal.classList.remove('is-active');
+    });
+
+    // Close modal when clicking on the modal background
+    modal.addEventListener('click', (event) => {
+        if (event.target.classList.contains('modal-background')) {
+            modal.classList.remove('is-active');
+        }
+    });
+}();
+
+//
+// Load animation
+//
+
+!function () {
+    document.addEventListener('DOMContentLoaded', () => {
+        const mainContent = document.querySelector('.main-content');
+
+        // Get the full height of the main content
+        const contentHeight = mainContent.scrollHeight;
+
+        // Set the height to the full height, allowing the transition to animate it
+        mainContent.style.height = contentHeight + 'px';
+
+        // After the transition, make the content fully visible and reset height to auto
+        mainContent.addEventListener('transitionend', () => {
+            mainContent.style.height = 'auto';
+            mainContent.classList.add('visible'); // Allow content to be fully seen
+            document.body.style.overflow = 'auto'; // Enable scrolling
+        }, { once: true });
+    });
+}();
+
+!function () {
+    imageCollections.forEach((collection) => {
+        // Create circle
+        const carouselCircle = document.createElement("div");
+        carouselCircle.classList.add("story-item");
+
+        // Create circle image
+        const itemImage = document.createElement("img");
+        itemImage.classList.add("not-selectable");
+        itemImage.src = collection.previewImage;
+        itemImage.alt = collection.name;
+        carouselCircle.appendChild(itemImage);
+
+        // Make it clickable
+        carouselCircle.addEventListener("click", () => {
+            clearCollectionContent();
+            addCollectionContent(collection.images);
+        });
+        document.getElementById("collections").appendChild(carouselCircle);
+    });
+}();
+
+function clearCollectionContent() {
+    const columnsSection = document.getElementById("collection-content");
+    columnsSection.innerHTML = "";
+}
+
+function addCollectionContent(images) {
+    images.forEach((image) => {
+        const column = document.createElement("div");
+        column.classList.add("column", "is-4");
+
+        const card = document.createElement("div");
+        card.classList.add("card", "large");
+        addModalOnClick(card);
+        column.appendChild(card);
+
+        const cardImage = document.createElement("div");
+        cardImage.classList.add("card-image");
+        card.appendChild(cardImage);
+
+        const figure = document.createElement("figure");
+        figure.classList.add("image", "is-4by5");
+        cardImage.appendChild(figure);
+
+        const img = document.createElement("img");
+        img.src = image;
+        img.alt = "Image";
+        figure.appendChild(img);
+
+        document.getElementById("collection-content").appendChild(column);
+    });
+}
+
+function addModalOnClick(element) {
+    const modalImage = document.getElementById('modalImage');
+
+    element.addEventListener('click', (event) => {
+        if (event.target.tagName === 'IMG') {
+            modalImage.src = event.target.src;
+            modal.classList.add('is-active');
+        }
+    });
+}
+
+// 
+// Carousel Shadow
+// 
+
+!function () {
+    var content = document.querySelector('.story-carousel'),
+        wrapper = document.querySelector('.container'),
+        shadowTop = document.querySelector('.shadow--top'),
+        shadowBottom = document.querySelector('.shadow--bottom'),
+        contentScrollHeight = content.scrollWidth - wrapper.offsetWidth;
+
+    if (contentScrollHeight > 0) {
+        content.addEventListener('scroll', function () {
+            var currentScroll = this.scrollLeft / (contentScrollHeight);
+            shadowTop.style.opacity = currentScroll;
+            shadowBottom.style.opacity = 1 - currentScroll;
+        });
+    } else {
+        shadowTop.style.opacity = 0;
+        shadowBottom.style.opacity = 0;
+    }
+}();
+
+function copyText() {
+    var copyText = document.getElementById("email");
+    navigator.clipboard.writeText(copyText.innerText);
+
+    var tooltip = document.getElementById("tooltiptext");
+    tooltip.innerHTML = "Copied: " + copyText.innerText;
+}
+
+function outFunc() {
+    var tooltip = document.getElementById("tooltiptext");
+    tooltip.innerHTML = "Copy to clipboard";
+}
